e427021e5929f1c6334fec1e07a9c705ed6b2417
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-02-24 19:14:33 +0530

    added list to store objects of class Person

--- src/list/List.java
+++ src/list/List.java
@@ -1,4 +1,17 @@
 package list;
+import person.Person;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class List {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Person> list1 = new ArrayList<Person>();
+
+    public List(String firstName, String lastName, String emailID) {
+    }
+    public List() {
+        super();
+    }
+    int top = 0;
+    Person p;
 }
