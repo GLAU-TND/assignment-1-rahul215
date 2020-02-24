8c4cc2c205f3684a2832edcde518edaede3b2406
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-02-24 19:30:25 +0530

    completed our main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,23 @@
 package main;
+import person.Person;
+import list.List;
+import menu.Menu;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Menu m1 = new Menu();
+        List t2 = new List();
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Welcome  to contacts menu ::: " );
+        m1.menu(sc.nextInt());
+        String i = "y";
+        while(i.equals("y")) {
+            m1.menu(sc.nextInt());
+            System.out.println("want to try more y/n?");
+            sc.nextLine();
+            i = sc.nextLine();
+        }
+    }
 }
