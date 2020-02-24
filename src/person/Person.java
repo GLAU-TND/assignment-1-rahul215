8552fc0cddcacc0d6a65379555a4b5a2cab5273a
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-02-24 19:06:34 +0530

    added class Person fields

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,5 +1,13 @@
 package person;
 
-public class Person {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Person {
+    Scanner sc = new Scanner(System.in);
+    public String firstName;
+    public String lastName;
+    public String  emailID;
+    public ArrayList<String> phoneNumber = new ArrayList<String>();
+    public int size = 0;
 }
