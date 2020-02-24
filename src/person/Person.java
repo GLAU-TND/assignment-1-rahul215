cf3a0096d7dca233149573207907151610b9dae1
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-02-24 19:09:26 +0530

    added gettor,settor and constructors respective fields of class Person

--- src/person/Person.java
+++ src/person/Person.java
@@ -10,4 +10,46 @@ public class Person {
     public String  emailID;
     public ArrayList<String> phoneNumber = new ArrayList<String>();
     public int size = 0;
+    public Person() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public Person(String firstName, String lastName, String emailID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailID = emailID;
+    }
+    public void setPhoneNumber() {
+        String i = "y";
+        while(i.equals("y")) {
+            phoneNumber.add(sc.nextLine());
+            size++;
+            System.out.println("want to add another number y/n?");
+            i = sc.nextLine();
+        }
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
 }
