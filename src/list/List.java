e749dad375792cc2b6212ad5793ab0e6666c10c4
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-02-24 19:16:49 +0530

    added method addNewContact

--- src/list/List.java
+++ src/list/List.java
@@ -14,4 +14,17 @@ public class List {
     }
     int top = 0;
     Person p;
+    public void addNewContact() {
+        System.out.println("Enter first name ::");
+        String m = scan.nextLine();
+        System.out.println("Enter last name ::");
+        String v = scan.nextLine();
+        System.out.println("Enter emailId ::");
+        String c = scan.nextLine();
+        p = new Person(m,v,c);
+        System.out.println("Enter phoneNumber ::");
+        p.setPhoneNumber();
+        list1.add(p);
+        top++;
+    }
 }
